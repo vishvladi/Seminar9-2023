6d2d26932c700112aadf81919720e9f04b1725cb
vishvladi <vlad-vishnevski@mail.ru>
2023-07-12 23:08:15 +0300

    Четные числа

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,23 @@
 //все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
-
-
+int ReadInt(string message)
+{
+    System.Console.Write(message);  
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number; 
+}
+int m = ReadInt("Введите число M: ");
+int n = ReadInt("Введите число N: ");
+void PrintEvens(int m, int n)
+{
+    if (m > n)
+    return;
+    if (m % 2 == 0)
+    {
+    Console.Write($"{m} ");
+    }
+    PrintEvens(m+1,n);
+}
+PrintEvens(m,n);
+System.Console.WriteLine();
